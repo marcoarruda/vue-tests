e422faa479f98305943618f69b6051dc9036754a
Marco Arruda <marco.nc.arruda@gmail.com>
2021-06-28 11:33:41 -0400

    test.todo, xtest, test.skip

--- tests/unit/Users.spec.js
+++ tests/unit/Users.spec.js
@@ -2,11 +2,23 @@ import { shallowMount } from "@vue/test-utils";
 import Users from "@/views/Users.vue";
 
 describe("Users.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(Users, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+  // it("renders props.msg when passed", () => {
+  //   const msg = "new message";
+  //   const wrapper = shallowMount(Users, {
+  //     propsData: { msg },
+  //   });
+  //   expect(wrapper.text()).toMatch(msg);
+  // });
+  xtest("Must contain a title 'List of users'", () => {
+
+  });
+  xtest("If there is no user, show only a message", () => {
+
+  });
+  xtest("If there are users, show 5 users per page", () => {
+
+  });
+  xtest("Show page links after the current page content", () => {
+
   });
 });

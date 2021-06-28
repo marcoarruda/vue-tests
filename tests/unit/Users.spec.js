370a89fa91de886d2aba6b6b1671697b259cb754
Marco Arruda <marco.nc.arruda@gmail.com>
2021-06-28 11:29:58 -0400

    Create users spec and copy/paste from example

---
+++ tests/unit/Users.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from "@vue/test-utils";
+import Users from "@/components/Users.vue";
+
+describe("Users.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(Users, {
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});

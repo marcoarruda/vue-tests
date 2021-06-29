3db8120a1c0f66d6418246fd0f88b4598042fbf6
Marco Arruda <marco.nc.arruda@gmail.com>
2021-06-28 20:43:13 -0400

    2nd test

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1 ref="title">List of users</h1>
+    <p ref="noUserMsg">There is no user to be shown</p>
   </div>
 </template>
 
--- tests/unit/views/Users.spec.js
+++ tests/unit/views/Users.spec.js
@@ -9,4 +9,22 @@ describe("Users.vue", () => {
     expect(title.exists()).toBe(true);
     expect(title.html()).toContain("List of users");
   });
+
+  test("If there is no user, show only a message about it", () => {
+    const wrapper = shallowMount(Users);
+    const noUserMsg = wrapper.findComponent({ ref: "noUserMsg" });
+    expect(noUserMsg.exists()).toBe(true);
+  });
+
+  test("If there are users, do NOT show 'no users' msg", () => {
+    const wrapper = shallowMount(Users);
+    const noUserMsg = wrapper.findComponent({ ref: "noUserMsg" });
+    expect(noUserMsg.exists()).toBe(false);
+  });
+
+  test.todo("If there are users, show them");
+
+  test.todo("If there are more than 5 users, show only 5 per page");
+
+  test.todo("Show page links properly after the current page content");
 });

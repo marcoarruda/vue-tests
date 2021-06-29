767252a216744d2844d8b4f9d3a6bf5221496e22
Marco Arruda <marco.nc.arruda@gmail.com>
2021-06-28 20:20:12 -0400

    users component

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/users",
+    name: "Users",
+    // route level code-splitting
+    // this generates a separate chunk (users.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "users" */ "../views/Users.vue"),
+  },
 ];
 
 const router = new VueRouter({
